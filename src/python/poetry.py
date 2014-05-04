47156ff8000aa06cc8fa6839061877f426b63619
snakecharmer1024 <snakecharmer1024@gmail.com>
2014-05-03 22:48:29 -0700

    Normalized frequency matrix. Refactored and renamed

--- src/python/poetry.py
+++ src/python/poetry.py
@@ -1,8 +1,13 @@
 import nltk
 import re
 
+def cleaned_sentences_from_text(text):
+	text_without_urls = remove_urls_from_text(text)
+	sentences = nltk.sent_tokenize(text)
+	cleaned_sentences = clean_sentences(sentences)
+	return cleaned_sentences
 
-def remove_tiny_sentences(sentences):
+def clean_sentences(sentences):
 	digrx = re.compile('.*\\d.*')
 	for i in range(0,len(sentences)):
 		try:
@@ -10,16 +15,23 @@ def remove_tiny_sentences(sentences):
 		except:
 			pass
 		
+
 		words = nltk.word_tokenize(sentence)
+		
+		# remove short sentences
 		if len(words)<3:
 			sentences.remove(sentence)
 			continue
 
+		# remove words with numbers in them and punctuation
 		for word in words:
 			if re.match(digrx, word) != None:
 				words.remove(word)
+			if word == ',' or word == '.' or word == ':' or word == ';':
+				words.remove(word)
 				#print 'removing word: '+word
 
+		# reconstruct sentences
 		newSentence = ''
 		for word in words:
 			newSentence += word.lower() + ' '
@@ -29,14 +41,14 @@ def remove_tiny_sentences(sentences):
 			pass
 	return sentences
 
-def remove_urls(text):
+def remove_urls_from_text(text):
 	res = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 	if len(res) > 0:
 		print res
 	text = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text, re.S)
 	return text
 
-def same_sentence_frequency(sentences):
+def same_sentence_frequency_matrix(sentences):
 	frequency_matrix = {}
 
 	# for each sentence
@@ -44,33 +56,71 @@ def same_sentence_frequency(sentences):
 		# tokenize the sentence
 		words = nltk.word_tokenize(sentence)
 
+		seen_it = []
 		# for each word in the sentence
 		for word1 in words:
-			if not frequency_matrix.has_key(word1):
-				frequency_matrix[word1] = {}
+			seen_it_2 = []
 
-			# increment frequency for each other word in sentence
-			for word2 in words:
-				if word2 != word1:
-					if not frequency_matrix[word1].has_key(word2):
-						frequency_matrix[word1][word2] = 1
-					else:
-						frequency_matrix[word1][word2] = frequency_matrix[word1][word2] + 1
+			if word1 not in seen_it:
+				seen_it.append(word1)
+				if not frequency_matrix.has_key(word1):
+					frequency_matrix[word1] = {}
+				# increment frequency for each other word in sentence
+				for word2 in words:
+					if word2 not in seen_it_2:
+						seen_it_2.append(word2)
+						if not frequency_matrix[word1].has_key(word2):
+							frequency_matrix[word1][word2] = 1
+						else:
+							frequency_matrix[word1][word2] += 1
 
 	return frequency_matrix				
 
+def word_frequency_per_sentence(sentences):
+	frequency_map = {}
 
-with open( '/Users/ganesha/Code/Poetry/doc/texts/Bitcoin.txt', 'r' ) as infile:
+	for sentence in sentences:
+		words = nltk.word_tokenize(sentence)
+		seen_it = []
+		for word in words:
+			if not word in seen_it:
+				seen_it.append(word)
+				if not frequency_map.has_key(word):
+					frequency_map[word] = 1
+				else:
+					frequency_map[word] = frequency_map[word] + 1
+
+	return frequency_map
+
+def normalize_same_sentence_frequency_matrix(freqMat):
+	normedFreqMat = {}
+	# for each pair of words encountered
+	for word1 in freqMat.keys():
+		if not normedFreqMat.has_key(word1):
+			normedFreqMat[word1] = {} 
+		for word2 in freqMat.keys():
+			if freqMat[word1].has_key(word2):
+				# divide frequency by the frequency of (word1 union word2)
+				# given that you saw one of them, what's the probability of seeing the other one?
+				normedFreqMat[word1][word2] = float(freqMat[word1][word2]) / (freqMat[word1][word1] + freqMat[word2][word2] - freqMat[word1][word2])
+
+	return normedFreqMat
+
+
+
+with open( '/Users/ganesha/Code/Poetry/doc/texts/Two_Sentences.txt', 'r' ) as infile:
 	text = ''
 	for line in infile:
 		text += line
 
-text = remove_urls(text)
-sentences = nltk.sent_tokenize(text)
-sentences = remove_tiny_sentences(sentences)
-print sentences
+sentences = cleaned_sentences_from_text(text)
+print 'sentences: ' + str(sentences)
 
-freq_mat = same_sentence_frequency(sentences)
+freq_mat = same_sentence_frequency_matrix(sentences)
+print 'frequency matrix: ' + str(freq_mat)
 
-print freq_mat
+freq_map = word_frequency_per_sentence(sentences)
+print 'frequency map: ' + str(freq_map)
 
+normed_freq_mat = normalize_same_sentence_frequency_matrix(freq_mat)
+print 'normed frequency matrix: ' + str(normed_freq_mat)
